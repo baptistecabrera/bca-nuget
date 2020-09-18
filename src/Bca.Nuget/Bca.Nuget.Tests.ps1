7a739371d85c27783e985d012084e814da0d03af
Baptiste Cabrera <baptiste.cabrera@outlook.com>
2020-09-18 14:18:18 +0000

    Fixed a test on Test-NuspecManifest

--- src/Bca.Nuget/Bca.Nuget.Tests.ps1
+++ src/Bca.Nuget/Bca.Nuget.Tests.ps1
@@ -392,6 +392,7 @@ Describe "Test-NuspecManifest" {
     It "Testing Nuspec Manifest from Path" {
         try
         {
+            Import-Module Pscx -Force
             $Result = Test-NuspecManifest -Path $NuspecManifest
         }
         catch
@@ -404,6 +405,7 @@ Describe "Test-NuspecManifest" {
     It "Testing Nuspec Manifest from Nuspec" {
         try
         {
+            Import-Module Pscx -Force
             $Result = Test-NuspecManifest -Nuspec $Nuspec
         }
         catch
